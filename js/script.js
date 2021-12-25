88d30bd9ac802e07efd9151438a80195e03d6424
Natalya-ya <bochkova.kvasova@gmail.com>
2021-12-25 23:36:24 +1300

    Making the Nav btn works

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// FOOTER YEAR
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+//NAVIGATION BTN
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
