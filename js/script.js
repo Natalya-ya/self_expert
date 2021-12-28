65f8393f576cbef4bc5b479fc615a3270bf3e39d
Natalya-ya <bochkova.kvasova@gmail.com>
2021-12-28 21:44:58 +1300

    Smooth scrolling effect and close mobile nav

--- js/script.js
+++ js/script.js
@@ -10,11 +10,6 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 //ACCORDION BTN
-// let btnAccordionEl = document.querySelector(".btn--down");
-// let accordionDivEl = document.querySelector(".accordion-div");
-// btnAccordionEl.addEventListener("click", function () {
-//   accordionDivEl.classList.toggle("open");
-// });
 const icons = document.querySelectorAll(".btn--down");
 
 for (let icon of icons) {
@@ -33,3 +28,22 @@ for (let iconUp of iconsUp) {
     itemUp.classList.toggle("open");
   });
 }
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
